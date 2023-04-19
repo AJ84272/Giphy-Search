68d4d23bd2ced29cfd13dc801a49aa97615c4ec3
aj84272 <AnuragChauhan@JPWS15605.local>
2023-04-20 07:16:45 +0900

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { SearchGiphyComponent } from './components/search-giphy/search-giphy.component';
+import { UserImagesComponent } from './components/user-images/user-images.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchGiphyComponent,
+    UserImagesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
